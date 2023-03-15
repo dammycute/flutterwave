e2966cf9bf7cdbfed953190d8c3fb03898940914
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2023-03-15 04:17:14 +0100

    Update Fundwallet.py

--- Fundwallet.py
+++ Fundwallet.py
@@ -1,40 +1,46 @@
-import requests
 from django.conf import settings
-from django.shortcuts import redirect
-from rest_framework.views import APIView
-from .models import Wallet
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from .models import Transaction, Wallet
+import requests
+import uuid
 
-class FundWalletView(APIView):
-    def get(self, request):
-        # get the user and amount from the request data
-        user = request.user
-        amount = request.query_params.get('amount')
-
-        # check if the user has a wallet, create one if they don't
-        try:
-            wallet = Wallet.objects.get(user=user)
-        except Wallet.DoesNotExist:
-            wallet = Wallet.objects.create(user=user)
-
-        # generate a payment request URL using Flutterwave's API
-        response = requests.post('https://api.flutterwave.com/v3/payments',
-            headers={'Authorization': f'Bearer {settings.FLUTTERWAVE_SECRET_KEY}'},
-            json={
-                'tx_ref': f'wallet-funding-{wallet.id}',
-                'amount': amount,
-                'currency': 'NGN',
-                'redirect_url': request.build_absolute_uri('/payment/redirect/'),
-                'payment_options': 'card',
-                'customer': {
-                    'email': user.email,
-                    'name': user.get_full_name(),
-                },
-                'meta': {
-                    'wallet_id': wallet.id,
-                },
-            },
-        )
-        payment_url = response.json()['data']['link']
-
-        # redirect the user to the payment request URL
-        return redirect(payment_url)
+@login_required
+def fundwallet(request):
+    amount = 500 # or whatever amount you want to charge
+    transaction_ref = str(uuid.uuid4())
+    user_wallet = Wallet.objects.filter(user=request.user).first()
+    if user_wallet is None:
+        user_wallet = Wallet.objects.create(user=request.user)
+    # create a new transaction record in the database
+    transaction = Transaction.objects.create(
+        user=request.user,
+        wallet=user_wallet,
+        transaction_ref=transaction_ref,
+        amount=amount
+    )
+    # construct the URL for the Flutterwave payment page
+    payload = {
+        'tx_ref': transaction_ref,
+        'amount': amount,
+        'currency': 'NGN',
+        'redirect_url': request.build_absolute_uri(reverse('fundwallet-success')),
+        'payment_options': 'card',
+        'customer': {
+            'email': request.user.email,
+            'phonenumber': request.user.profile.phone_number,
+            'name': request.user.get_full_name()
+        },
+        'customizations': {
+            'title': 'My Store',
+            'description': 'Payment for wallet funding',
+            'logo': 'https://example.com/logo.png'
+        }
+    }
+    response = requests.post('https://api.flutterwave.com/v3/payments', headers={
+        'Authorization': f'Bearer {settings.FLUTTERWAVE_SECRET_KEY}',
+        'Content-Type': 'application/json'
+    }, json=payload)
+    # redirect the user to the Flutterwave payment page
+    return redirect(response.json()['data']['link'])
